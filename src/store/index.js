6914b2338a12b8f4c3b2accff52744c327a05aea
adostos <ado19960@gmail.com>
2020-12-16 12:08:54 +0100

    apikey / vuex -> if con

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        api_key: '01d173fa50230c4e27143afd37a8ce86',
+        api_key: '806d90eba26abce6511d4c2daa4da2a1',
         query: '',
+        count: 0,
         towns: [
             { 'name': 'Bratislava', 'lon': 17.11, 'lat': 48.15 },
             { 'name': 'Košice', 'lon': 21.33, 'lat': 48.67 },
@@ -22,24 +23,31 @@ export default new Vuex.Store({
     mutations: {
         searchCity(state, value) {
 			state.query = value
-		},
+        },
+        increment (state) {
+            state.count += 1
+        }
     },
     actions: {
         getWeatherData(context) {
-            context.state.weather = []
-            context.state.towns.map(town => {
-                axios
-                    .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${town.lat}&lon=${town.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${context.state.api_key}`)
-                    .then(response => {
-                        let weatherResponse = response.data
-                        weatherResponse.name = town.name
-                        weatherResponse.country = "Slovakia"
-                        context.state.weather.push(weatherResponse)
-                    })
-                    .catch(error => {
-                        alert('Please wait...', error)
-                    })
-            })
+            // vypnut if pre stalu aktualizaciu
+            if (context.state.count === 0) {
+                context.state.weather = []
+                context.state.towns.map(town => {
+                    axios
+                        .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${town.lat}&lon=${town.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${context.state.api_key}`)
+                        .then(response => {
+                            let weatherResponse = response.data
+                            weatherResponse.name = town.name
+                            weatherResponse.country = "Slovakia"
+                            context.state.weather.push(weatherResponse)
+                        })
+                        .catch(error => {
+                            alert('Please wait...', error)
+                        })
+                });
+                context.commit('increment')
+            }
         },
         
     },
